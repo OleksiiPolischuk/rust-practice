f8266e5615879453eecec912c8503d63b9fb6774
Олексій <oleksii.polishchuk@e-u.edu.ua>
2025-03-20 12:20:51 +0200

    Перезаписав hw03.rs

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,1 +1,48 @@
+const WIDTH: usize = 30;
+const HEIGHT: usize = 16;
+
+fn main() {
+    let mut output = String::new();
+
+    // Верхня межа
+    output.push_str(&("*".repeat(WIDTH) + "\n"));
+
+    // Малювання верхньої половини
+    for i in 0..(HEIGHT / 2 - 1) {
+        output.push('*');
+        output.push_str(&" ".repeat(2 * i));
+        output.push('*');
+        output.push_str(&" ".repeat(WIDTH - 4 - 4 * i));
+        output.push('*');
+        output.push_str(&" ".repeat(2 * i));
+        output.push('*');
+        output.push('\n');
+    }
+
+    // Центр конверта
+    output.push('*');
+    output.push_str(&" ".repeat(WIDTH / 2 - 2));
+    output.push_str("**");
+    output.push_str(&" ".repeat(WIDTH / 2 - 2));
+    output.push('*');
+    output.push('\n');
+
+    // Нижня половина 
+    for i in (0..(HEIGHT / 2 - 1)).rev() {
+        output.push('*');
+        output.push_str(&" ".repeat(2 * i));
+        output.push('*');
+        output.push_str(&" ".repeat(WIDTH - 4 - 4 * i));
+        output.push('*');
+        output.push_str(&" ".repeat(2 * i));
+        output.push('*');
+        output.push('\n');
+    }
+
+    // Нижня межа
+    output.push_str(&("*".repeat(WIDTH) + "\n"));
+
+    // Вивід
+    print!("{}", output);
+}
  
